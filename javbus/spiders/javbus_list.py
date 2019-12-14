3a3634d7899818b5e7ba09249d1082d82d8a6bf5
zhoubing <zhoubing19820720@hotmail.com>
2019-12-14 08:14:38 +0800

    splash集成

--- javbus/spiders/javbus_list.py
+++ javbus/spiders/javbus_list.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Request
+from scrapy_splash import SplashRequest
 
 
 class JavbusListSpider(scrapy.Spider):
     name = 'javbus_list'
     allowed_domains = ['buscdn.work']
+
     # start_urls = ['http://buscdn.work/']
 
     def start_requests(self):
@@ -51,38 +53,50 @@ class JavbusListSpider(scrapy.Spider):
             print(bango)
             print(date)
 
-            yield Request("https://www.buscdn.work/page/2", callback=self.parse_detail,
-                          meta= {
-                              "item": {
-                                  "url": url,
-                                  "img_url": img_url,
-                                  "img_title": img_title,
-                                  "title": title,
-                                  "is_hd_link_flag": is_hd_link_flag,
-                                  "is_hd_link_title": is_hd_link_title,
-                                  "is_hd_link_text": is_hd_link_text,
-
-                                  "is_magnet_link_flag": is_magnet_link_flag,
-                                  "is_magnet_link_title": is_magnet_link_title,
-                                  "is_magnet_link_text": is_magnet_link_text,
-
-                                  "bango": bango,
-                                  "date": date
-                              }
-                          }, dont_filter=True)
+            yield SplashRequest(url, self.parse_splash_detail, args={
+                'wait': 0.5,
+            })
+            # yield Request("https://www.buscdn.work/page/2", callback=self.parse_detail,
+            #               meta={
+            #                   "item": {
+            #                       "url": url,
+            #                       "img_url": img_url,
+            #                       "img_title": img_title,
+            #                       "title": title,
+            #                       "is_hd_link_flag": is_hd_link_flag,
+            #                       "is_hd_link_title": is_hd_link_title,
+            #                       "is_hd_link_text": is_hd_link_text,
+            #
+            #                       "is_magnet_link_flag": is_magnet_link_flag,
+            #                       "is_magnet_link_title": is_magnet_link_title,
+            #                       "is_magnet_link_text": is_magnet_link_text,
+            #
+            #                       "bango": bango,
+            #                       "date": date
+            #                   }
+            #               }, dont_filter=True)
         pass
+
+    def parse_splash_detail(self, response):
+        print(response.xpath("//table[@id='magnet-table']//td//@href").extract_first())
 
     def parse_detail(self, response):
         title = response.xpath("//div[@class='container']/h3/text()").extract_first()
         big_image = response.xpath("//div[@class='col-md-9 screencap']/a[@class='bigImage']//img/@src").extract_first()
-        no_bango_title = response.xpath("//div[@class='col-md-9 screencap']/a[@class='bigImage']//img/@title").extract_first()
-        bango = response.xpath("//div[@class='col-md-3 info']//span[contains(text(), '識別碼')]/following-sibling::*/text()").extract_first()
-        date = response.xpath("//div[@class='col-md-3 info']//span[contains(text(), '發行日期')]/parent::*/text()").extract_first()
-        length = response.xpath("//div[@class='col-md-3 info']//span[contains(text(), '長度')]/parent::*/text()").extract_first()
-        company = response.xpath("//div[@class='col-md-3 info']//span[contains(text(), '製作商')]/parent::*/a/text()").extract_first()
+        no_bango_title = response.xpath(
+            "//div[@class='col-md-9 screencap']/a[@class='bigImage']//img/@title").extract_first()
+        bango = response.xpath(
+            "//div[@class='col-md-3 info']//span[contains(text(), '識別碼')]/following-sibling::*/text()").extract_first()
+        date = response.xpath(
+            "//div[@class='col-md-3 info']//span[contains(text(), '發行日期')]/parent::*/text()").extract_first()
+        length = response.xpath(
+            "//div[@class='col-md-3 info']//span[contains(text(), '長度')]/parent::*/text()").extract_first()
+        company = response.xpath(
+            "//div[@class='col-md-3 info']//span[contains(text(), '製作商')]/parent::*/a/text()").extract_first()
         distributor = response.xpath(
             "//div[@class='col-md-3 info']//span[contains(text(), '發行商')]/parent::*/a/text()").extract_first()
-        director = response.xpath("//div[@class='col-md-3 info']//span[contains(text(), '導演')]/parent::*/a/text()").extract_first()
+        director = response.xpath(
+            "//div[@class='col-md-3 info']//span[contains(text(), '導演')]/parent::*/a/text()").extract_first()
 
         genres = response.xpath(
             "//div[@class='col-md-3 info']//p[contains(text(), '類別')]/following-sibling::*/span[@class='genre']//text()")
